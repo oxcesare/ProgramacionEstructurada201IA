fa64782514c47db6f6d65aa3af8263286b9b702e
Cesar <cesar180687@hotmail.com>
2026-03-13 10:06:16 -0600

    Ejercicios match y lógicos

---
+++ src/ux.com.curso.programacion/unidad1/estructura_repeticion/ejercicio_match.py
@@ -0,0 +1,27 @@
+# Implementacion de Match en Python
+
+def demostracion():
+    print("-- Ejemplo de match --")
+    opcion = input("Ingrese una opción (1-3): ")
+
+    match opcion:
+        case "1":
+            print("Opción 1 seleccionada")
+            nombre = input("Ingrese su nombre: ")            
+            print(f"Hola, {nombre}!")
+        case "2":
+            print("Opción 2 seleccionada")    
+            matricula = input("Ingrese su matrícula: ")
+            print(f"Su matrícula es: {matricula}")
+        case "3":
+            print("Opción 3 seleccionada")
+            semestre = input("Ingrese su semestre: ")
+            print(f"Usted está en el semestre: {semestre}")
+        case _:
+            print("Opción no válida")    
+
+def main():
+    demostracion()
+
+if __name__ == "__main__":
+    main()
