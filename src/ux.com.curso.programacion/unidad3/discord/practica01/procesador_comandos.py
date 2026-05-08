92bba08bfd2b89c0eaa49728fb44f096f0e41a40
Cesar <cesar180687@hotmail.com>
2026-05-08 10:38:11 -0600

    Lógica discord

--- src/ux.com.curso.programacion/unidad3/discord/practica01/procesador_comandos.py
+++ src/ux.com.curso.programacion/unidad3/discord/practica01/procesador_comandos.py
@@ -61,9 +61,15 @@ def iniciar_agente():
             print(obtener_saludo(NOMBRE_BOT))
         elif comando == "ayuda":
             print(mostrar_ayuda())    
+        elif comando == "recordar":
+            print(procesar_comando_recordar(argumento))           
+        elif comando == "salir":
+            print("¡Hasta luego!")
+            ejecutando = False      
+        elif comando == "uptime":
+            print(calcular_uptime(hora_inicio))              
         else:
             print("Comando no reconocido.")
-
             
 def main():
     iniciar_agente()
