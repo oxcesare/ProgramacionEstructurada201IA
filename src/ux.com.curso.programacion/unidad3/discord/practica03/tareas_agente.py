b78063a90d3a4e52721e1ede3fcfdac0a4d23ebc
Cesar <cesar180687@hotmail.com>
2026-05-11 08:21:50 -0600

    Ejercicios Básicos Certificación Pyhton: Operadores

--- src/ux.com.curso.programacion/unidad3/discord/practica03/tareas_agente.py
+++ src/ux.com.curso.programacion/unidad3/discord/practica03/tareas_agente.py
@@ -13,3 +13,18 @@ def agregar_tarea(lista_tareas,descripcion):
     nueva_tarea = f"{descripcion} - {fecha}"
     lista_tareas.append(nueva_tarea)
     return f"Tarea agregada con exito"
+
+def listar_tareas(lista_tareas):
+    """
+     Formatea la lista de tareas para su visualización
+    """
+    if not lista_tareas:
+        return "No hay tareas"
+    
+    # agrega una variable llamada resultado
+    resultado = "Listado de tareas: \n"
+
+    #Iterar la lista de tareas y formatear la salida
+    for i, tarea in enumerate(lista_tareas, start=1):
+        resultado += f"{i}. {tarea}\n"
+    return resultado   
