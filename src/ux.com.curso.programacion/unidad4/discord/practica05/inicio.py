a3554c554c759aad66b29babff63cfc53851d823
Cesar <cesar180687@hotmail.com>
2026-06-07 10:08:36 -0600

    Ejercicios Discord

--- src/ux.com.curso.programacion/unidad4/discord/practica05/inicio.py
+++ src/ux.com.curso.programacion/unidad4/discord/practica05/inicio.py
@@ -1,20 +1,30 @@
 import discord
 import os
 import re
+import sys
 from dotenv import load_dotenv
+# Importación relativa limpia (Subir a 'discord' y bajar a 'practica04')
+from ..practica04.tareas_agente import agregar_tarea, listar_tareas, eliminar_tarea
+
+tareas = []  # Nuestra "base de datos" en memoria (lista)
+
 
 def mostrar_bienvenida():
     """Retorna la lista de comandos disponibles."""
     return (
         "📜 Bot de Gestión de Tareas (Programacion Estructurada):\n"
-        "📜 Primeros pasos Agente Discord UX:\n"        
-        "📜 Esccriba !Exit para salir del Agente:"
+        "📜 Primeros pasos Agente Discord UX:\n"  
+        "📜 Escriba !add <tarea> para agregar una tarea:\n"        
+        "📜 Escriba !list para listar las tareas:\n"
+        "📜 Escriba !delete <tarea> para eliminar una tarea:\n"
+        "📜 Escriba !exit para salir del Agente:"
     )
 
 def main(entrada):
     
         PREFIJO = "!"
         
+
         if not entrada.startswith(PREFIJO):
             if entrada: print("Recuerda usar '!' para comandos.")
             
@@ -26,7 +36,17 @@ def main(entrada):
         # Selección de acción (Estructura de control)
         if comando == "exit":
             print("Saliendo del gestor...")
-            return "Saliendo del gestor..."        
+            return "Saliendo del gestor..."
+
+        elif comando == "add":            
+            return agregar_tarea(tareas, argumento)
+        
+        elif comando == "list":
+            return listar_tareas(tareas)
+        
+        elif comando == "delete":
+            return eliminar_tarea(tareas, argumento)
+
         else:
             print(f" Error: Comando '!{comando}' no reconocido.")
             return f" Error: Comando '!{comando}' no reconocido."
@@ -63,9 +83,7 @@ async def on_message(message):
         await message.channel.send(bienvenida)
         return
     
-    #invocar la función principal para procesar comandos
-
-
+    #invocar la funcion main para procesar los comandos
         
     # 3. Procesamiento: Pasamos el contenido del mensaje a nuestra lógica
     print(f"Mensaje recibido de {message.author}: {message.content}")
